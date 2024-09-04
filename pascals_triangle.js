f914ba15ff7aeab1d752191179f769176f96d448
sureshsubedii <sureshsubedi485@gmail.com>
2024-09-04 21:14:16 +0545

    Find Pascals traingle

---
+++ pascals_triangle.js
@@ -0,0 +1,21 @@
+// Given an integer numRows, return the first numRows of Pascal's triangle.
+// In Pascal's triangle, each number is the sum of the two numbers directly above it
+
+var generate = function(numRows) {
+    let prev = []
+    let result = []
+    result.push([1])
+
+    for(let i = 1; i < numRows; i ++){
+        prev = result[ i - 1]
+        console.log(prev)
+        let newRow = new Array(i + 1)
+        newRow[0] = newRow[i] = 1
+        for(let j = 1; j < i; j ++){
+            newRow[j] = prev[j - 1] +  prev[j ]
+        }
+        result.push(newRow)
+    }
+    return result
+    
+};
