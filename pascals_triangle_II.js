82f4c8aacaf3f0f44ed8ab7cfecd3c16c9db4563
suresh_11 <sureshsubedi485@gmail.com>
2024-09-05 11:06:13 +0545

    Optimize pascal's  triangle  to use only O(n) extra space

--- pascals_triangle_II.js
+++ pascals_triangle_II.js
@@ -1,18 +1,17 @@
 var getRow = function(rowIndex) {
-    let result = [[1]]
     let prev  = []
 
-    for(let i = 1; i < rowIndex; i++){
+    for(let i = 0; i <= rowIndex; i++){
         let newRow = []
         newRow[0] = newRow[i] = 1
-        prev = result[ i - 1]
         for(let j = 1; j < i; j ++ ){
             newRow[j] = prev[ j - 1] + prev[j]
 
         }
+        prev = newRow
 
 
     }
-    return result[rowIndex]
+    return prev
     
 };
