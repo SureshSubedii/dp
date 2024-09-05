4f7e97ca374e0c97695673b8c04437a05e895dad
sureshsubedii <sureshsubedi485@gmail.com>
2024-09-05 21:01:02 +0545

    Optimise isSubsequence function to work for large inputs

--- is_subsequence.js
+++ is_subsequence.js
@@ -2,18 +2,45 @@
 // A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) 
 // of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
-var isSubsequence = function(s, t) {
-    let i = 0
-    let j = 0
-    while(j < t.length){
-        if(s[i] === t[j]){
-            i ++
-            j ++
-        }else{
-            j ++
-        }
+// Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if 
+// t has its subsequence. In this scenario, how would you change your code?
 
+function preprocessT(t) {
+    const indexMap = new Map();
+    for (let i = 0; i < t.length; i++) {
+        if (!indexMap.has(t[i])) {
+            indexMap.set(t[i], []);
+        }
+        indexMap.get(t[i]).push(i);
     }
-    return i === s.length
-    
-};
+    return indexMap;
+}
+
+function isSubsequence(si, indexMap) {
+    let currentPosition = -1; 
+    for (const char of si) {
+        if (!indexMap.has(char)) {
+            return false; 
+        }
+        const indices = indexMap.get(char);
+        const pos = binarySearch(indices, currentPosition);
+        if (pos === indices.length) {
+            return false; 
+        }
+        currentPosition = indices[pos];
+    }
+    return true;
+}
+
+function binarySearch(array, target) {
+    let left = 0, right = array.length;
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+        if (array[mid] <= target) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return left;
+}
